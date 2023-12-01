6a90626e21c46ac958e0c57a515c5c70036d35c2
Geoff Schmit <gcschmit@gmail.com>
2023-12-01 09:26:19 -0600

    add createEntry route, page, script

---
+++ assets/js/createEntry.js
@@ -0,0 +1,16 @@
+/**
+ * contains client-side JavaScript function
+ *  (primarily event handlers to fetch data from the Node server)
+ */
+
+const submitButton = document.querySelector("input.submit");
+submitButton.addEventListener("click", () => {
+  // get the values entered by the user
+  const date = document.querySelector("input.date").value;
+  const habitOfMind = document.querySelector("input.HoM").value;
+  const content = document.querySelector("textarea.content").value;
+  console.log(date + "; " + habitOfMind + "; " + content);
+
+  // for now, redirect to the home page
+  window.location = "/";
+});
