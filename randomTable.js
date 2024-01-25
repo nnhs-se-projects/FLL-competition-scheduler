4ffce045891da3a2d3f4296814219c218e0a5352
SophiaRXi <111532254+SophiaRXi@users.noreply.github.com>
2024-01-25 08:43:35 -0600

    Table assignment (#3)
    * finish getRandomNum function and create team list
    
    * adding teams to tables
    
    * start checking for team booked at the same time
    
    * fix minor error with count
    
    * draft of spacing teams code
    
    * adding time in between teams
    
    * undo spacing teams edits
    
    ---------
    
    Co-authored-by: Kelsey Wessel <klwessel@stu.naperville203.org>

---
+++ randomTable.js
@@ -0,0 +1,63 @@
+let teams = [];
+let table1 = [];
+let table2 = [];
+let table3 = [];
+let table4 = [];
+for (let i = 0; i < 3; i++) {
+  for (let i = 1; i < 33; i++) {
+    let str = "Team " + i;
+    teams.push(str);
+  }
+}
+let length = teams.length;
+function getRandomTeam() {
+  let randomTeam = Math.floor(Math.random() * teams.length);
+  return teams[randomTeam];
+}
+let count = 1;
+for (let i = 0; i < length; i++) {
+  let team = getRandomTeam();
+  let index = teams.indexOf(team);
+  if (count === 1) {
+    table1.push(team);
+    count++;
+  } else if (count === 2) {
+    if (team != table1[table1.length - 1]) {
+      table2.push(team);
+      count++;
+    } else {
+      i--;
+      continue;
+    }
+  } else if (count === 3) {
+    if (
+      team != table1[table1.length - 1] &&
+      team != table2[table2.length - 1]
+    ) {
+      table3.push(team);
+      count++;
+    } else {
+      i--;
+      continue;
+    }
+  } else if (count === 4) {
+    if (
+      team != table1[table1.length - 1] &&
+      team != table2[table2.length - 1] &&
+      team != table3[table3.length - 1]
+    ) {
+      table4.push(team);
+      count++;
+      count = 1;
+    } else {
+      i--;
+      continue;
+    }
+  }
+  teams.splice(index, 1);
+}
+console.log(teams);
+console.log("table 1: " + table1);
+console.log("table 2: " + table2);
+console.log("table 3: " + table3);
+console.log("table 4: " + table4);
