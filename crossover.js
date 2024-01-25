0816e1534d1fd1f6393c13610684d246dec31962
Kelsey Wessel <klwessel@stu.naperville203.org>
2024-01-25 09:20:22 -0600

    started working on crossover method for genetic algorithm

---
+++ crossover.js
@@ -0,0 +1,16 @@
+import { randomTable } from "./randomTable.js";
+
+const POPULATION = 10;
+
+const newPool = [];
+const oldPool = [];
+
+function crossover() {
+
+let parentA = randomTable();
+let parentB = randomTable();
+
+let x1 = 
+
+
+}
--- randomTable.js
+++ randomTable.js
@@ -1,63 +1,69 @@
-let teams = [];
-let table1 = [];
-let table2 = [];
-let table3 = [];
-let table4 = [];
-for (let i = 0; i < 3; i++) {
-  for (let i = 1; i < 33; i++) {
-    let str = "Team " + i;
-    teams.push(str);
-  }
-}
-let length = teams.length;
-function getRandomTeam() {
-  let randomTeam = Math.floor(Math.random() * teams.length);
-  return teams[randomTeam];
-}
-let count = 1;
-for (let i = 0; i < length; i++) {
-  let team = getRandomTeam();
-  let index = teams.indexOf(team);
-  if (count === 1) {
-    table1.push(team);
-    count++;
-  } else if (count === 2) {
-    if (team != table1[table1.length - 1]) {
-      table2.push(team);
-      count++;
-    } else {
-      i--;
-      continue;
-    }
-  } else if (count === 3) {
-    if (
-      team != table1[table1.length - 1] &&
-      team != table2[table2.length - 1]
-    ) {
-      table3.push(team);
-      count++;
-    } else {
-      i--;
-      continue;
-    }
-  } else if (count === 4) {
-    if (
-      team != table1[table1.length - 1] &&
-      team != table2[table2.length - 1] &&
-      team != table3[table3.length - 1]
-    ) {
-      table4.push(team);
-      count++;
-      count = 1;
-    } else {
-      i--;
-      continue;
+function randomTable() {
+  let teams = [];
+  let table1 = [];
+  let table2 = [];
+  let table3 = [];
+  let table4 = [];
+  for (let i = 0; i < 3; i++) {
+    for (let i = 1; i < 33; i++) {
+      let str = "Team " + i;
+      teams.push(str);
     }
   }
-  teams.splice(index, 1);
+  let length = teams.length;
+  function getRandomTeam() {
+    let randomTeam = Math.floor(Math.random() * teams.length);
+    return teams[randomTeam];
+  }
+  let count = 1;
+  for (let i = 0; i < length; i++) {
+    let team = getRandomTeam();
+    let index = teams.indexOf(team);
+    if (count === 1) {
+      table1.push(team);
+      count++;
+    } else if (count === 2) {
+      if (team != table1[table1.length - 1]) {
+        table2.push(team);
+        count++;
+      } else {
+        i--;
+        continue;
+      }
+    } else if (count === 3) {
+      if (
+        team != table1[table1.length - 1] &&
+        team != table2[table2.length - 1]
+      ) {
+        table3.push(team);
+        count++;
+      } else {
+        i--;
+        continue;
+      }
+    } else if (count === 4) {
+      if (
+        team != table1[table1.length - 1] &&
+        team != table2[table2.length - 1] &&
+        team != table3[table3.length - 1]
+      ) {
+        table4.push(team);
+        count++;
+        count = 1;
+      } else {
+        i--;
+        continue;
+      }
+    }
+    teams.splice(index, 1);
+  }
+
+  let tables = [table1, table2, table3, table4];
+  return tables;
+
+  //console.log(teams);
+  //console.log("table 1: " + table1);
+  //console.log("table 2: " + table2);
+  //console.log("table 3: " + table3);
+  //console.log("table 4: " + table4);
 }
-console.log(teams);
-console.log("table 1: " + table1);
-console.log("table 2: " + table2);
-console.log("table 3: " + table3);
-console.log("table 4: " + table4);
