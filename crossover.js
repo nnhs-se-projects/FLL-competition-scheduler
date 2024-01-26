392de4f7f114bf38d6a57ad5a4b7324942554bb2
SophiaRXi <srxi@stu.naperville203.org>
2024-01-26 09:35:10 -0600

    added to crossover\

--- crossover.js
+++ crossover.js
@@ -5,12 +5,39 @@ const POPULATION = 10;
 const newPool = [];
 const oldPool = [];
 
-function crossover() {
+function crossOver(x1, x2, parentA, parentB) {
+  let children = [];
+  let child1 = [];
+  let child2 = [];
+  let c1t1 = [];
+  let c1t2 = [];
+  let c1t3 = [];
+  let c1t4 = [];
+  let c2t1 = [];
+  let c2t2 = [];
+  let c2t3 = [];
+  let c2t4 = [];
 
-let parentA = randomTable();
-let parentB = randomTable();
-
-let x1 = 
-
-
+  for (let i = 0; i < x1; i++) {
+    c1t1.push(parentA[0][i]);
+  }
+  for (let i = x1; i < x2; i++) {
+    c1t1.push(parentB[0][i]);
+  }
+  for (let i = x2; i < parentA[0].length; i++) {
+    c1t1.push(parentA[0][i]);
+  }
+  console.log(c1t1);
 }
+
+function geneticAlgorithm() {
+  let parentA = randomTable();
+  let parentB = randomTable();
+
+  let x1 = Math.floor((Math.random() * parentA[0].length) / 2 + 1);
+  let x2 = Math.floor(Math.random() * (parentA[0].length / 2) + x1);
+
+  crossOver(x1, x2, parentA, parentB);
+}
+
+geneticAlgorithm();
