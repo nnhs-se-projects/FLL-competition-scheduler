441ee24a0ce6ad4c83ec194b70330ec934a00e88
angelajping <ajping@stu.naperville203.org>
2024-01-26 08:52:44 -0600

    start genetic algorithm code and make randomJS into a function

---
+++ JRcrossover.js
@@ -0,0 +1,13 @@
+import { randomJS } from "./randomJudgingSesh.js";
+
+const POPULATION = 10;
+
+const newPool = [];
+const oldPool = [];
+
+function crossover() {
+  let parentA = randomJS();
+  let parentB = randomJS();
+
+  // let x1 =
+}
