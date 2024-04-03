f429573479f12b3ba3e34273e82ea83b346c1474
apandya14 <111534834+apandya14@users.noreply.github.com>
2024-04-03 10:17:53 -0500

    Judging room crossover (#7)
    * start genetic algorithm code and make randomJS into a function
    
    * Determining the child based off of chosen 3 crossover points
    
    * slightly edit random function and crossover
    
    * start crossover method, end before correcting children
    
    * importing functions into test using require()
    
    * children does not print at all in test
    
    * add console.log back into test
    
    * try debugging
    
    * try to debug futher, figure out import functions error
    
    * checking infinite loop
    
    * attempt to fix error, write check duplicates function
    
    * judgingSesh test
    
    * debugging and searching for exceptions
    
    * debugging crossover
    
    * revise generating checkpoint index
    
    * work on replaceDuplicates function in crossover
    
    * finish replace duplicates for crossover!
    
    * fix getRandomNumx1 and x2
    
    * JrGrading (#6)
    
    * finish jrGrading method and test
    
    * performGeneticAlg creates old pool and new pool
    
    * write genetic algorithm so that it comes up with an optimized schedule
    
    * add some debugging for genetic alg
    
    * check grade before pushing crossover
    
    * ensure that the pools only contain sched w higher scores
    
    * updating the grading
    
    * fix error of not creating pool
    
    * updating JRGrading for improved scoring
    
    * final grading system edits
    
    ---------
    
    Co-authored-by: apandya14 <ajpandya@stu.naperville203.org>
    
    ---------
    
    Co-authored-by: angelajping <ajping@stu.naperville203.org>
    Co-authored-by: angela ping <74382001+angelajping@users.noreply.github.com>

---
+++ JRcrossover.js
@@ -0,0 +1,131 @@
+import { randomJS } from "./randomJudgingSesh.js";
+// const randomJS = require("./randomJudgingSesh.js");
+
+const POPULATION = 10;
+
+function getRandomNumX1(parent) {
+  return Math.floor(Math.random() * (parent[0].length / 2) + 1);
+}
+
+function getRandomNumX2(parent) {
+  return Math.floor(
+    Math.random() * (parent[0].length / 2 - 1) + parent[0].length / 2 + 1
+  );
+}
+
+let judgingRoomNum = 8;
+
+// x1 and x2 are cross over points
+function crossover(x1, x2, parentA, parentB) {
+  // make sure x1 is greater than x2 and by a min amount otherwise there is no crossover in the middle section
+  const length = parentA.length;
+
+  /// create empty children, array of empty JR arrays
+  let childA = [];
+  for (let i = 0; i < judgingRoomNum; i++) {
+    childA.push([]);
+  }
+  let childB = [];
+
+  for (let i = 0; i < judgingRoomNum; i++) {
+    childB.push([]);
+  }
+
+  // copy the 1st segment of parentA to the childA
+  for (let room = 0; room < parentA.length; room++) {
+    for (let i = 0; i < x1; i++) {
+      childA[room].push(parentA[room][i]); // Fix: Replace [] with ()
+    }
+  }
+  //console.log("Child A: " + childA);
+  // copy the 1st segment of parentB to the childB
+  for (let room = 0; room < parentB.length; room++) {
+    for (let i = 0; i < x1; i++) {
+      childB[room].push(parentB[room][i]);
+    }
+  }
+
+  // copy the middle section of parentA to child B
+  for (let room = 0; room < parentA.length; room++) {
+    for (let i = x1; i < x2; i++) {
+      childB[room].push(parentA[room][i]);
+    }
+  }
+  // copy the middle section of parentB to child A
+  for (let room = 0; room < parentB.length; room++) {
+    for (let i = x1; i < x2; i++) {
+      childA[room].push(parentB[room][i]);
+    }
+  }
+
+  // copy the last segment of parentA to childA
+  for (let room = 0; room < parentA.length; room++) {
+    for (let i = x2; i < parentA[room].length; i++) {
+      childA[room].push(parentA[room][i]);
+    }
+  }
+  // copy the last segment of parentB to childB
+  for (let room = 0; room < parentB.length; room++) {
+    for (let i = x2; i < parentB[room].length; i++) {
+      childB[room].push(parentB[room][i]);
+    }
+  }
+
+  // let children = [childA, childB];
+  // console.log("Children BEFORE RD: ");
+  // console.log("Child A: ");
+  // for (let i = 0; i < children.length / 2; i++) {
+  //   for (let j = 0; j < children[i].length; j++) {
+  //     if (j < children[i].length / 2) {
+  //       console.log("Robot Room " + (j + 1) + ": " + children[i][j]);
+  //     } else {
+  //       console.log("Project Room " + (j + 1) + ": " + children[i][j]);
+  //     }
+  //   }
+  // }
+
+  // console.log("Child B: ");
+  // for (let i = children.length / 2; i < children.length; i++) {
+  //   for (let j = 0; j < children[i].length; j++) {
+  //     if (j < children[i].length / 2) {
+  //       console.log("Robot Room " + (j + 1) + ": " + children[i][j]);
+  //     } else {
+  //       console.log("Project Room " + (j + 1) + ": " + children[i][j]);
+  //     }
+  //   }
+  // }
+  let children = replaceDuplicates(childA, childB, x1, x2);
+
+  //console.log("Children: " + children);
+  return children;
+
+  // add the child to the new pool
+}
+
+function replaceDuplicates(childA, childB, x1, x2) {
+  // correct/get rid of duplicates, add teams that got lost in the crossover sections
+  // look at the crossed over section and scan for teams A previously had but B didn't
+  // and vice versa
+  // switch the teams they didn't have in common
+  let constB = childB;
+  for (let room = 0; room < childA.length; room++) {
+    for (let a = x1; a < x2; a++) {
+      if (!constB.includes(childA[room][a])) {
+        for (let b = x1; b < x2; b++) {
+          if (!childA.includes(childB[room][b])) {
+            let swap = childA[room][a];
+            childA[room][a] = childB[room][b];
+            childB[room][b] = swap;
+          }
+        }
+      }
+    }
+  }
+  return [childA, childB];
+}
+
+export { replaceDuplicates };
+export { crossover };
+export { getRandomNumX1 };
+export { getRandomNumX2 };
+// after everything, determine criteria for good child and give scores to the children
---
+++ randomJudgingSesh.js
@@ -0,0 +1,126 @@
+// number of teams (must be configurable)
+// number of judging rooms (should be configurable; start with 8)
+// number of judging sessions per team (should be configurable from 1 to 3; start with 2)
+// length of judging sessions (should be configurable, start with 15 minutes)
+// time between judging sessions (should be configurable; start with 10 minutes)
+
+// import { replaceDuplicates } from "./JRcrossover";
+
+function randomJS() {
+  //NOTE: make numTeams a param later then need to change jrGrading method
+  let teamsR = [];
+  let teamsP = [];
+  let judgingSeshs = 2;
+  let numTeams = 32;
+  for (let i = 1; i < numTeams + 1; i++) {
+    let str = "team" + i;
+    teamsR.push(str);
+    teamsP.push(str);
+  }
+
+  function getRandomNum() {
+    return Math.floor(Math.random() * teamsR.length); // doesnt matter if its teamsR or teamsP because they are the same length
+  }
+
+  let judgingRoomsRobot = 4;
+  let judgingRoomsProj = 4;
+  let robotRooms = [];
+  let projectRooms = [];
+
+  for (let i = 0; i < judgingRoomsRobot; i++) {
+    robotRooms.push([]);
+  }
+  for (let i = 0; i < judgingRoomsProj; i++) {
+    projectRooms.push([]);
+  }
+
+  function checkDuplicates(timeSlot) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (projectRooms[i][timeSlot] === robotRooms[j][timeSlot]) {
+          let rand = Math.floor(Math.random() * (timeSlot + 1));
+          const teamSwap = robotRooms[j][timeSlot];
+          //need to check index for conflicts
+          for (let r = 0; r < robotRooms.length; r++) {
+            if (robotRooms[r][rand] === teamSwap) {
+              rand = Math.floor(Math.random() * (timeSlot + 1));
+            }
+          }
+          for (let r = 0; r < projectRooms.length; r++) {
+            if (projectRooms[r][rand] === teamSwap) {
+              rand = Math.floor(Math.random() * (timeSlot + 1));
+            }
+          }
+          robotRooms[j][timeSlot] = robotRooms[0][rand];
+          robotRooms[0][rand] = teamSwap;
+        }
+      }
+    }
+  }
+  function checkIndex(timeSlot) {
+    let pr = -1;
+    for (let i = 0; i < robotRooms.length; i++) {
+      for (let j = 0; j < timeSlot; j++) {
+        for (let room = 0; room < projectRooms.length; room++) {
+          if (projectRooms[room].includes(robotRooms[i][j])) {
+            pr = projectRooms[room].indexOf(robotRooms[i][j]);
+          }
+        }
+        if (pr != -1 && Math.abs(pr - j) === 1) {
+          let rand = Math.floor(Math.random() * (timeSlot + 1));
+          while (Math.abs(rand - j) === 1) {
+            rand = Math.floor(Math.random() * (timeSlot + 1));
+          }
+          let teamSwap = robotRooms[i][j];
+          robotRooms[i][j] = robotRooms[0][rand];
+          robotRooms[0][rand] = teamSwap;
+        }
+      }
+    }
+  }
+
+  let timeSlot = 0;
+  let count = 0;
+  while (teamsR.length > 0 || teamsP.length > 0) {
+    for (let i = 0; i < robotRooms.length; i++) {
+      let randomNum = getRandomNum();
+      let team = teamsR[randomNum];
+      robotRooms[i].push(team);
+      teamsR.splice(randomNum, 1);
+    }
+    for (let i = 0; i < projectRooms.length; i++) {
+      let randomNum = getRandomNum();
+      let team = teamsP[randomNum];
+      projectRooms[i].push(team);
+      count++;
+      teamsP.splice(randomNum, 1);
+    }
+    timeSlot++;
+    checkDuplicates(timeSlot - 1);
+    if (timeSlot > 3) {
+      checkIndex(timeSlot - 1);
+    }
+  }
+
+  // for (let i = 0; i < robotRooms.length; i++) {
+  //   console.log("Robot Room " + (i + 1) + ": " + robotRooms[i]);
+  // }
+  // for (let i = 0; i < projectRooms.length; i++) {
+  //   console.log("Project Room " + (i + 1) + ": " + projectRooms[i]);
+  // }
+
+  let rooms = robotRooms.concat(projectRooms);
+  return rooms;
+}
+
+//console.log(randomJS());
+
+// for (let i = 0; i < robotRooms.length; i++) {
+//   console.log("Robot Room " + (i + 1) + ": " + robotRooms[i]);
+// }
+// for (let i = 0; i < projectRooms.length; i++) {
+//   console.log("Project Room " + (i + 1) + ": " + projectRooms[i]);
+// }
+
+export { randomJS };
+//export { checkDuplicates };
