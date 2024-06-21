be19bb09496f9c76f953acebb9cd8a0c792b3c40
Geoff Schmit <gcschmit@gmail.com>
2024-06-21 14:12:29 -0500

    start to debug crossovers

--- crossover.js
+++ crossover.js
@@ -189,14 +189,14 @@ function crossOver(parentA, parentB) {
       const tableRunA = tableScheduleA[i][j];
       const tableRunB = tableScheduleB[i][j];
       if (tableRunA.start < x1) {
-        childA[0][i].push(tableRunA);
-        childB[0][i].push(tableRunB);
+        childA[0][i].push(cloneTableRun(tableRunA));
+        childB[0][i].push(cloneTableRun(tableRunB));
       } else if (tableRunA.start < x2) {
-        childA[0][i].push(tableRunB);
-        childB[0][i].push(tableRunA);
+        childA[0][i].push(cloneTableRun(tableRunB));
+        childB[0][i].push(cloneTableRun(tableRunA));
       } else {
-        childA[0][i].push(tableRunA);
-        childB[0][i].push(tableRunB);
+        childA[0][i].push(cloneTableRun(tableRunA));
+        childB[0][i].push(cloneTableRun(tableRunB));
       }
     }
   }
@@ -212,17 +212,19 @@ function crossOver(parentA, parentB) {
       const sessionA = judgingScheduleA[i][j];
       const sessionB = judgingScheduleB[i][j];
       if (sessionA.startT < x1) {
-        childA[1][i].push(sessionA);
-        childB[1][i].push(sessionB);
+        childA[1][i].push(cloneJudgingSession(sessionA));
+        childB[1][i].push(cloneJudgingSession(sessionB));
       } else if (sessionA.startT < x2) {
-        childA[1][i].push(sessionB);
-        childB[1][i].push(sessionA);
+        childA[1][i].push(cloneJudgingSession(sessionB));
+        childB[1][i].push(cloneJudgingSession(sessionA));
       } else {
-        childA[1][i].push(sessionA);
-        childB[1][i].push(sessionB);
+        childA[1][i].push(cloneJudgingSession(sessionA));
+        childB[1][i].push(cloneJudgingSession(sessionB));
       }
     }
   }
+
+  // FIXME: update start times and durations for table runs and judging sessions
 
   console.log("Parent A");
   logJudgingRoomsSchedule(parentA);
@@ -242,228 +244,75 @@ function crossOver(parentA, parentB) {
   console.log("Child B");
   logTableRunsSchedule(childB);
 
+  replaceDuplicates(parentA, parentB, childA, x1, x2);
+  replaceDuplicates(parentB, parentA, childB, x1, x2);
+
+  // FIXME: update start times and durations for table runs and judging sessions
+
   return [childA, childB];
-
-  let child1 = [c1t1, c1t2, c1t3, c1t4];
-  let child2 = [c2t1, c2t2, c2t3, c2t4];
-  let rChild1 = replaceDuplicates(child1, x1, x2);
-  let rChild2 = replaceDuplicates(child2, x1, x2);
-  return [rChild1, rChild2];
 }
 
-function replaceDuplicates(child, x1, x2) {
-  let duplicates = [];
-  let missing = [];
-  let loops = 0;
-  let loops2 = 0;
-  // adds to a temporary array each time a specific team is found. Then checks and adds any missing or duplicates
-  // repeats for each team
-  for (let t = 1; t < 33; t++) {
-    let temp = [];
-    for (let i = 0; i < child.length; i++) {
-      for (let j = 0; j < child[i].length; j++) {
-        if (child[i][j].name === "team" + t) {
-          let arr = [i, j, child[i][j]];
-          temp.push(arr);
-        }
-      }
-    }
-    // checks for duplicates and missing teams
-    let run1 = 0;
-    let run2 = 0;
-    let run3 = 0;
-    for (let i = 0; i < temp.length; i++) {
-      if (temp[i][2].run === 1) {
-        run1++;
-      } else if (temp[i][2].run === 2) {
-        run2++;
-      } else if (temp[i][2].run === 3) {
-        run3++;
-      }
-    }
-    if (run1 > 1) {
-      for (let k = 0; k < temp.length; k++) {
-        if (temp[k][2].run === 1 && temp[k][1] >= x1 && temp[k][1] < x2) {
-          duplicates.push(temp[k]);
-        }
-      }
-    } else if (run1 < 1) {
-      let str = { name: "team" + t, run: 1 };
-      missing.push(str);
-    }
-
-    if (run2 > 1) {
-      for (let j = 0; j < temp.length; j++) {
-        if (temp[j][2].run === 2 && temp[j][1] >= x1 && temp[j][1] < x2) {
-          duplicates.push(temp[j]);
-        }
-      }
-    } else if (run2 < 1) {
-      let str = { name: "team" + t, run: 2 };
-      missing.push(str);
-    }
-
-    if (run3 > 1) {
-      for (let i = 0; i < temp.length; i++) {
-        if (temp[i][2].run === 3 && temp[i][1] >= x1 && temp[i][1] < x2) {
-          duplicates.push(temp[i]);
-        }
-      }
-    } else if (run3 < 1) {
-      const str = { name: "team" + t, run: 3 };
-      missing.push(str);
-    }
-  }
-
-  function swap(tableNum) {
-    // swaps a duplicate with a missing team
-    while (loops2 < 100) {
-      loops2++;
-      const rand = Math.floor(Math.random() * (x2 - x1) + x1);
-      const temp = child[tableNum][rand];
-      if (
-        missing.length > 0 &&
-        child[0][rand].name !== missing[0].name &&
-        child[1][rand].name !== missing[0].name &&
-        child[2][rand].name !== missing[0].name &&
-        child[3][rand].name !== missing[0].name
-      ) {
-        child[tableNum][rand] = missing[0];
-        missing[0] = temp;
-        return "done";
-      } else if (x1 > 1 && x2 < 22) {
+function replaceDuplicates(parentA, parentB, child, x1, x2) {
+  let uniqueTableRuns = [];
+  // iterate through each table in parent A; this assumes that the start times for each run are consistent across all schedules
+  for (let i = 0; i < parentA[0].length; i++) {
+    // iterate through each table run at the table and focus on those in the cross-over segment
+    for (let j = 0; j < parentA[0][i].length; j++) {
+      const tableRunA = parentA[0][i][j];
+      if (tableRunA.start >= x1 && tableRunA.start < x2) {
         if (
-          child[0][rand].name !== temp.name &&
-          child[1][rand].name !== temp.name &&
-          child[2][rand].name !== temp.name &&
-          child[3][rand].name !== temp.name &&
-          child[0][rand - 1].name !== temp.name &&
-          child[1][rand - 1].name !== temp.name &&
-          child[2][rand - 1].name !== temp.name &&
-          child[3][rand - 1].name !== temp.name &&
-          child[0][rand + 1].name !== temp.name &&
-          child[1][rand + 1].name !== temp.name &&
-          child[2][rand + 1].name !== temp.name &&
-          child[3][rand + 1].name !== temp.name
+          countTeamTableRunsInTimePeriod(parentB[0], tableRunA, x1, x2) === 0
         ) {
-          child[tableNum][rand] = missing[0];
-          missing[0] = temp;
-          return "done";
-        }
-      }
-    }
-    if (loops2 === 100) {
-      return null;
-    }
-  }
-
-  let length = duplicates.length;
-
-  if (length < 5) {
-    if (
-      duplicates.length === 2 &&
-      duplicates[0][2].run === duplicates[1][2].run
-    ) {
-      duplicates.splice(0, 1);
-      length = duplicates.length;
-    }
-    for (let i = 0; i < length; i++) {
-      // swaps all duplicates if there are less than 5
-      const index = duplicates[i][1];
-      if (
-        missing.length > 0 &&
-        x1 > 1 &&
-        x2 < 22 &&
-        missing[0].name !== child[0][index].name &&
-        missing[0].name !== child[1][index].name &&
-        missing[0].name !== child[2][index].name &&
-        missing[0].name !== child[3][index].name &&
-        missing[0].name !== child[0][index - 1].name &&
-        missing[0].name !== child[1][index - 1].name &&
-        missing[0].name !== child[2][index - 1].name &&
-        missing[0].name !== child[3][index - 1].name &&
-        missing[0].name !== child[0][index + 1].name &&
-        missing[0].name !== child[1][index + 1].name &&
-        missing[0].name !== child[2][index + 1].name &&
-        missing[0].name !== child[3][index + 1].name
-      ) {
-        child[duplicates[i][0]][duplicates[i][1]] = missing[0];
-        missing.splice(0, 1);
-        // goes to else if when x1 is greater than 1 and x2 is less than 22
-      } else if (
-        missing.length > 0 &&
-        missing[0].name !== child[0][index].name &&
-        missing[0].name !== child[1][index].name &&
-        missing[0].name !== child[2][index].name &&
-        missing[0].name !== child[3][index].name
-      ) {
-        child[duplicates[i][0]][duplicates[i][1]] = missing[0];
-        missing.splice(0, 1);
-      } else {
-        let swapResult = swap(duplicates[i][0]);
-        if (swapResult === "done") {
-          i--;
-          continue;
-        } else if (swapResult === null) {
-          return null;
-        }
-      }
-    }
-  } else {
-    for (let i = 0; i < length - 4; i++) {
-      // swaps all but 4 duplicates
-      if (loops > 100) {
-        return null;
-      }
-      let rand = Math.floor(Math.random() * missing.length);
-      const index = duplicates[i][1];
-      if (
-        missing[rand].name !== undefined &&
-        missing[rand].name !== child[0][index].name &&
-        missing[rand].name !== child[1][index].name &&
-        missing[rand].name !== child[2][index].name &&
-        missing[rand].name !== child[3][index].name
-      ) {
-        child[duplicates[i][0]][duplicates[i][1]] = missing[rand];
-        missing.splice(rand, 1);
-      } else {
-        loops++;
-        i--;
-        continue;
-      }
-    }
-    if (missing.length <= 3) {
-      return null;
-    }
-    for (let i = length - 4; i < length; i++) {
-      // swaps the last 4 duplicates
-      const index = duplicates[i][1];
-      if (
-        missing.length > 0 &&
-        missing[0].name !== child[0][index].name &&
-        missing[0].name !== child[1][index].name &&
-        missing[0].name !== child[2][index].name &&
-        missing[0].name !== child[3][index].name
-      ) {
-        child[duplicates[i][0]][duplicates[i][1]] = missing[0];
-        missing.splice(0, 1);
-      } else {
-        let swapResult = swap(duplicates[i][0]);
-        if (swapResult === "done") {
-          i--;
-          continue;
-        } else if (swapResult === null) {
-          return null;
+          uniqueTableRuns.push(tableRunA);
         }
       }
     }
   }
-  return child;
-}
 
-for (let i = 0; i < 1; i++) {
-  console.log("run", i + 1);
-  run();
+  // iterate through each table in the child; this assumes that the start times for each run are consistent across all schedules
+  for (let i = 0; i < child[0].length; i++) {
+    // iterate through each table run at the table and focus on those in the first and third segments
+    for (let j = 0; j < child[0][i].length; j++) {
+      const tableRun = child[0][i][j];
+      if (tableRun.start < x1 || tableRun.start >= x2) {
+        if (countTeamTableRunsInTimePeriod(child[0], tableRun, x1, x2) > 0) {
+          child[0][i][j] = uniqueTableRuns.pop();
+        }
+      }
+    }
+  }
+
+  let uniqueJudgingSessions = [];
+  // iterate through each judging room in parent A; this assumes that the start times for each run are consistent across all schedules
+  for (let i = 0; i < parentA[0].length; i++) {
+    // iterate through each session in the judging room and focus on those in the cross-over segment
+    for (let j = 0; j < parentA[0][i].length; j++) {
+      const session = parentA[0][i][j];
+      if (session.startT >= x1 && session.startT < x2) {
+        if (
+          countTeamJudgingSessionsInTimePeriod(parentB[0], session, x1, x2) ===
+          0
+        ) {
+          uniqueJudgingSessions.push(session);
+        }
+      }
+    }
+  }
+
+  // iterate through each judging room in the child; this assumes that the start times for each run are consistent across all schedules
+  for (let i = 0; i < child[0].length; i++) {
+    // iterate through each session in the judging room and focus on those in the first and third segments
+    for (let j = 0; j < child[0][i].length; j++) {
+      const session = child[0][i][j];
+      if (session.startT < x1 || session.startT >= x2) {
+        if (
+          countTeamJudgingSessionsInTimePeriod(child[0], session, x1, x2) > 0
+        ) {
+          child[0][i][j] = uniqueJudgingSessions.pop();
+        }
+      }
+    }
+  }
 }
 
 function mutate(crossoverChildren) {
@@ -516,4 +365,71 @@ function makeEmptySchedule() {
     schedule[1].push([]);
   }
   return schedule;
+}
+
+function countTeamTableRunsInTimePeriod(
+  tablesSchedule,
+  tableRun,
+  startTime,
+  endTime
+) {
+  let count = 0;
+  for (let i = 0; i < tablesSchedule.length; i++) {
+    for (let j = 0; j < tablesSchedule[i].length; j++) {
+      if (
+        tablesSchedule[i][j].id === tableRun.id &&
+        tablesSchedule[i][j].run === tableRun.run &&
+        tablesSchedule[i][j].start >= startTime &&
+        tablesSchedule[i][j].start < endTime
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function countTeamJudgingSessionsInTimePeriod(
+  judgingSessionsSchedule,
+  session,
+  startTime,
+  endTime
+) {
+  let count = 0;
+  for (let i = 0; i < judgingSessionsSchedule.length; i++) {
+    for (let j = 0; j < judgingSessionsSchedule[i].length; j++) {
+      if (
+        judgingSessionsSchedule[i][j].id === session.id &&
+        judgingSessionsSchedule[i][j].type === session.type &&
+        judgingSessionsSchedule[i][j].start >= startTime &&
+        judgingSessionsSchedule[i][j].start < endTime
+      ) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function cloneTableRun(tableRun) {
+  return {
+    id: tableRun.id,
+    run: tableRun.run,
+    start: tableRun.start,
+    duration: tableRun.duration,
+  };
+}
+
+function cloneJudgingSession(session) {
+  return {
+    id: session.id,
+    type: session.type,
+    startT: session.startT,
+    duration: session.duration,
+  };
+}
+
+for (let i = 0; i < 1; i++) {
+  console.log("run", i + 1);
+  run();
 }
