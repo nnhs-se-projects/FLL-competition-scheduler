a30e88f90e42b0c1f59ad7aa8194726d1c7c006f
“Mona <arnavsharma0904@gmail.com>
2025-01-21 10:42:37 -0600

    added function to compile schedule to JSON object

--- fullRandomScheduleTest.js
+++ fullRandomScheduleTest.js
@@ -1,8 +1,13 @@
-import { createFullSchedule, scoreSchedule } from "./fullRandomSchedule.js";
+import {
+  createFullSchedule,
+  printSchedule,
+  scoreSchedule,
+} from "./fullRandomSchedule.js";
 
 for (let i = 0; i < 100; i++) {
   console.log("Test " + i);
   let schedule = createFullSchedule();
   console.log("schedule score: " + scoreSchedule(schedule));
+  printSchedule(schedule);
 }
 console.log("test passed");
